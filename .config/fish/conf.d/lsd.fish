b24f7ba1b3e4fc8b73efb0ceaac08f40595e5305
dthelegend <me@daudi.dev>
2023-06-04 02:24:17 +0100

    Move to FISH SHELL

---
+++ .config/fish/conf.d/lsd.fish
@@ -0,0 +1,7 @@
+alias ls='lsd'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+alias pacman='sudo pacman'
+alias startdocker='systemctl start docker'
+alias stopdocker='systemctl stop docker'
